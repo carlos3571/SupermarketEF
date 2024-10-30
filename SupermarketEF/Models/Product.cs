43d02bc267b934f6b7603230d7d30e8c09f1574d
Carlos Orozco <carlosandresorozcoillera@gmail.com>
2024-10-30 14:39:03 -0500

    Inicio de proyecto

---
+++ SupermarketEF/Models/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupermarketEF.Models
+{
+    public class Product
+    {
+        // [Key] -> Anotacion si la propiedad no se llama Id, ejemplo ProductIf
+        public int Id { get; set; } // Sera la llave primaria
+
+        public string Name { get; set; }
+
+        [Column(TypeName = "decimal(6,2)")]
+
+        public decimal Price { get; set; }
+
+        public int Stock { get; set; }
+
+        public int CategoryId { get; set; } // Sera la llave foranea
+
+        public Category Category { get; set; } // Propiedad de navegacion
+
+    }
+}
---
+++ SupermarketEF/Program.cs
@@ -0,0 +1,63 @@
+﻿using SupermarketEF.Data;
+using SupermarketEF.Models;
+
+
+
+    using SupermarketContext context = new SupermarketContext();
+
+
+    // Se crea objeto oilCategory de tipo Category
+    Category oilCategory = new Category()
+
+    {
+        Name = "Oil",
+        Description = "Oil Category"
+    };
+
+    // Se agrega el objeto creado al contexto de la DB
+    // Usando la propieda Categories del contxto
+
+    context.Categories.Add(oilCategory);
+
+    // Se crea el objeto grainsCategory de tipo Category
+    Category grainsCategory = new Category()
+    {
+
+        Name = "Grains",
+        Description = "Grains Category"
+    };
+
+    // Se agrega el objeto crado al contexto de la DB
+    context.Add(grainsCategory);
+
+    // Se graban los cambios reaizados en el contexto
+    context.SaveChanges();
+
+    // Recupera todas las categorias y las ordena por nombre
+    var categories = context.Categories.OrderBy(p => p.Name);
+    foreach (var category in categories)
+    {
+        Console.WriteLine($"{category.Name} | {category.Description}");
+    }
+
+
+
+/*
+var grainCategory = context.Categories.Where(p => p.Name == "Grains").FirstOrDefault();
+
+if (grainCategory is Category)
+{
+    grainCategory.Description = "New description applied";
+    context.Remove(grainCategory); // Borra registro
+}
+
+context.SaveChanges();
+
+var categories = context.Categories.OrderBy(p => p.Name);
+
+foreach (var category in categories)
+{
+    Console.WriteLine($"{category.Name} | {category.Description}");
+}
+
+*/
